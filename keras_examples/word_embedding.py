74aa642bf2979558b43d58df85316e5ebe98c275
Guy Feldman <gfeldman8@gmial.com>
2017-09-15 16:16:30 -0400

    Keras Examples

---
+++ keras_examples/word_embedding.py
@@ -0,0 +1,15 @@
+# TextGen
+# 
+# Created by: Guy Feldman <gfeldman8@gmail.com>
+# Created: 9/14/17
+#
+# ID: word_embedding.py
+
+from keras.datasets import imdb
+from keras import preprocessing
+
+
+
+# cut texts after this number of words
+maxlen = 20
+
